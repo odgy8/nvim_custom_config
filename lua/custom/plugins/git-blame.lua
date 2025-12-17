8db0d157bf1bc5022e5cc9d74c6bad87e0e1be43
SamHodg1993 <samhodgkinson9192@gmail.com>
2025-12-17 19:41:09 +0000

    Update to address small issue with git blame placement

--- lua/custom/plugins/git-blame.lua
+++ lua/custom/plugins/git-blame.lua
@@ -6,7 +6,7 @@ return {
 	opts = {
 		-- your configuration comes here
 		enabled = true, -- if you want to enable the plugin
-		message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message
+		message_template = "<author> • <summary> • <date> • <<sha>>", -- template for the blame message
 		date_format = "%m-%d-%Y %H:%M:%S", -- template for the date
 		virtual_text_column = 1, -- virtual text start column
 	},
